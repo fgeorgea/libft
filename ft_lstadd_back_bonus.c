5c5b3acee3ddb661dc9f096d889db83e9ced0373
Felix Georgeault <fgeorgea@fu-r8-p6.s19.be>
2022-10-11 19:36:31 +0200

    add some stuff

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: fgeorgea <fgeorgea@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 17:48:35 by fgeorgea          #+#    #+#             */
-/*   Updated: 2022/10/11 16:58:52 by fgeorgea         ###   ########.fr       */
+/*   Updated: 2022/10/11 19:04:00 by fgeorgea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,13 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
 
-	if (!new)
+	if (!new || !lst)
 		return ;
 	if (!*lst)
+	{
 		*lst = new;
+		return ;
+	}
 	else
 	{
 		last = ft_lstlast(*lst);
--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: fgeorgea <fgeorgea@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 18:08:54 by fgeorgea          #+#    #+#             */
-/*   Updated: 2022/10/11 16:59:27 by fgeorgea         ###   ########.fr       */
+/*   Updated: 2022/10/11 19:36:17 by fgeorgea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
-	if (!lst || !*del)
+	if (!lst || !del)
 		return ;
-	(*del)(lst->content);
+	del(lst->content);
 	free(lst);
 }
