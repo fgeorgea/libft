cf02a7a904ca8815cfcf1c74e7693a9a7660a544
Felix georgeault <gorgo.felix@gmail.com>
2023-06-24 23:38:55 +0200

    fixed printf for mac silicon and modified perror

--- display/ft_printf.c
+++ display/ft_printf.c
@@ -3,31 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fgeorgea <fgeorgea@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fgeorgea <fgeorgea@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 19:31:32 by fgeorgea          #+#    #+#             */
-/*   Updated: 2023/06/19 13:18:34 by fgeorgea         ###   ########.fr       */
+/*   Updated: 2023/06/24 23:33:22 by fgeorgea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+#include <stdio.h>
 
-static void	ft_check_type(int fd, va_list ap, char type, int *len)
+static void	ft_check_type(int fd, va_list *ap, char type, int *len)
 {
 	if (type == 'c')
-		*len += ft_printchar(fd, va_arg(ap, int));
+		*len += ft_printchar(fd, va_arg(*ap, int));
 	if (type == 's')
-		*len += ft_printstr(fd, va_arg(ap, char *));
+		*len += ft_printstr(fd, va_arg(*ap, char *));
 	if (type == 'p')
-		*len += ft_printptr(fd, va_arg(ap, unsigned long int));
+		*len += ft_printptr(fd, va_arg(*ap, unsigned long int));
 	if (type == 'd' || type == 'i')
-		*len += ft_printnbr(fd, va_arg(ap, int));
+		*len += ft_printnbr(fd, va_arg(*ap, int));
 	if (type == 'u')
-		*len += ft_printnbr(fd, va_arg(ap, unsigned int));
+		*len += ft_printnbr(fd, va_arg(*ap, unsigned int));
 	if (type == 'x')
-		ft_printhex(fd, va_arg(ap, unsigned int), "0123456789abcdef", len);
+		ft_printhex(fd, va_arg(*ap, unsigned int), "0123456789abcdef", len);
 	if (type == 'X')
-		ft_printhex(fd, va_arg(ap, unsigned int), "0123456789ABCDEF", len);
+		ft_printhex(fd, va_arg(*ap, unsigned int), "0123456789ABCDEF", len);
 	if (type == '%')
 		*len += ft_printchar(fd, '%');
 }
@@ -47,7 +48,7 @@ int	ft_printf(int fd, const char *str, ...)
 	{
 		if (str[i] == '%')
 		{
-			ft_check_type(fd, ap, str[i + 1], &len);
+			ft_check_type(fd, &ap, str[i + 1], &len);
 			i++;
 		}
 		else
--- error/ft_perror.c
+++ error/ft_perror.c
@@ -6,23 +6,16 @@
 /*   By: fgeorgea <fgeorgea@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 20:20:35 by fgeorgea          #+#    #+#             */
-/*   Updated: 2023/06/24 23:20:36 by fgeorgea         ###   ########.fr       */
+/*   Updated: 2023/06/24 23:36:57 by fgeorgea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
-#include <stdio.h>
 
 void	ft_perror(const char *message)
 {
-	char	*err_mess;
-
-	err_mess = ft_strerror(errno);
 	if (message)
-	{
-		ft_putstr_fd((char *)message, 2);
-		ft_putstr_fd(": ", 2);
-	}
-	ft_putstr_fd(err_mess, 2);
-	ft_putstr_fd("\n", 2);
+		ft_printf(2, "%s: %s\n", message, ft_strerror(errno));
+	else
+		ft_printf(2, "%s\n", ft_strerror(errno));
 }
